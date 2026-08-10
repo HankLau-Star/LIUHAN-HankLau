2ef3c481357a79227323425b41bafedd123dc078
LIUHAN/HankLau <veritasrensheng@gmail.com>
2026-08-10 20:20:24 +0800

    Remove incorrectly flattened GitHub OAuth route

--- app/api/auth/github/route.ts
+++
@@ -1,7 +0,0 @@
-import { endGitHubSession } from "../../../../../lib/github-oauth";
-
-export const dynamic = "force-dynamic";
-
-export async function GET(request: Request) {
-  return endGitHubSession(request);
-}
